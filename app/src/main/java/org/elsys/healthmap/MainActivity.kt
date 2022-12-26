16d3fbb3c8e173e75aee92eab1703120f9cafb94
0DakatA0 <dstoilov199@gmail.com>
2022-12-26 16:35:31 +0200

    [Add/Updeate] Initialize repository

---
+++ app/src/main/java/org/elsys/healthmap/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.elsys.healthmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
